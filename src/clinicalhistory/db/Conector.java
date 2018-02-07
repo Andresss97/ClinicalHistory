09268cf63fbd3aece289ae276d7a0db0a4c87cd6
Adriana <a.torres37@usp.ceu.es>
2018-02-07 12:50:34 +0100

    initial project

---
+++ src/clinicalhistory/db/Conector.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clinicalhistory.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author andre
+ */
+public class Conector {
+    private Connection connect;
+
+    public Connection getConnect() {
+        return connect;
+    }
+
+    public void setConnect(Connection connect) {
+        this.connect = connect;
+    }
+    
+    public void conectar() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connect = DriverManager.getConnection("jdbc:sqlite:" + "C:\\DatabasesGHC\\DBproject.db");
+        }
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void killConnection() {
+        try {
+            connect.close();
+        }
+        catch(SQLException ex) {
+            Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
