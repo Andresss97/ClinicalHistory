ca8796e73a311593d5dce962ea17929cb88c3ffc
Daniel Marchan <danielmarchan3@gmail.com>
2018-02-18 19:42:58 +0100

    class illness

---
+++ src/pojos/Illness.java
@@ -0,0 +1,22 @@
+package pojos;
+
+import java.sql.Date;
+
+public class Illness {
+	
+	
+	private String description;
+	private Date date_disease; 
+	public enum typeDiseases{HEREDITARY,PERSONAL};
+	private typeDiseases type;
+	private String name;
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
