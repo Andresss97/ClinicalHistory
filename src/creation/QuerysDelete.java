858d769597d8941af22ff988f7d9f56a4f654677
Adriana <a.torres37@usp.ceu.es>
2018-03-16 15:42:25 +0100

    UML diagram

--- src/creation/QuerysDelete.java
+++ src/creation/QuerysDelete.java
@@ -1,10 +1,34 @@
 package creation;
 
-import pojos.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import graphicInterface.Main;
+import pojos.*;
 public class QuerysDelete {
 
-	private static void deleteAppointment (Appointment appointment) {
+	private Conector conn = Main.conector;
+	
+	
+	private void deleteAppointment (Appointment appointment) {
 		
+		String query;
+		query = "DELETE FROM Appointment WHERE id = ?";
+		PreparedStatement st;
+		try {
+			st = conn.getConnect().prepareStatement(query);
+			st.setInt(1, appointment.getID());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void deleteClinicalHistory (ClinicalHistory clinicalHistory) {
+		String query;
+		query = "DELETE FROM ClinicalHistory WHERE id = ?";
+		
+		PreparedStatement st;
+		st.getConnection().prepareStatement(query);
 	}
 }
