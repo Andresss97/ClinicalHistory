c51970da8b48e66453ca8c4590ca84774523edc2
Levi <levihango@gmail.com>
2018-04-06 16:24:15 +0200

    vaccineAnnotations

--- src/pojos/Vaccine.java
+++ src/pojos/Vaccine.java
@@ -1,18 +1,29 @@
 package pojos;
-import java.io.*;
+
 import java.sql.Date;
 
-public class Vaccine implements Serializable{
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "vaccines")
+
+public class Vaccine {
+	@Id
+	@GeneratedValue(generator = "vaccines")
+	@TableGenerator(name = "vaccines", table = "sqlite_sequence",
+		pkColumnName = "name", valueColumnName = "seq", pkColumnValue = "vaccines")
 	private Integer IDvaccine;
 	public enum typeVaccine{CHOLERA,DIPHTHERIA,INFLUENZA_A,INFLUENZA_B,HEPATITIS_A,HEPATITIS_B,PAPILLOMAVIRUS,
 		HERPES,MEASLES,MENINGOCOCCAL,PNEUMOCOCCAL,RABIES,ROTAVIRUS,RUBELLA,SMALLPOX,TETANUS,TUBERCULOSIS,TYPHOID,
 		VARICELLA,YELLOWFEVER};
+		
 	private typeVaccine nameVaccine;
 	private Date date;
 	private String description;
 	private Patient patient;
 	private Treatment treatment;
-	
+
 	public Vaccine() {
 		this.IDvaccine = 0;
 		this.nameVaccine = null;
