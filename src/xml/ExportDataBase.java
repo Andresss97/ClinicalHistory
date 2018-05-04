d18a6a6f9dfbc84ddf2a7c30b2c9305375511f15
Adriana <a.torres37@usp.ceu.es>
2018-05-04 17:04:10 +0200

    Whole database export to XML

--- src/xml/ExportDataBase.java
+++ src/xml/ExportDataBase.java
@@ -11,13 +11,21 @@ public class ExportDataBase {
 	
 	private DataBaseMarshaller marshaller;
 	
-	public void export (XmlLists lists, List<Doctor>doctors, List<Patient>patients, List<ClinicalHistory>clinicalHistories, List<Appointment>appointments, File file) throws JAXBException {
+	public void export (XmlLists lists, List <Address> addresses, List <Allergies> allergies, List <Appointment> appointments,
+	  List <ClinicalHistory> clinicalHistories, List <Doctor> doctors, List <Illness> illnesses, List <Patient> patients, 
+	  List <Surgeries> surgeries, List <Treatment> treatments, List <Vaccine> vaccines, File file) throws JAXBException {
 		
-		lists.setDoctors(doctors);
-		lists.setPatients(patients);
-		lists.setClinicalHistories(clinicalHistories);
+		lists.setAddresses(addresses);
+		lists.setAllergies(allergies);
 		lists.setAppointments(appointments);
-	    
+		lists.setClinicalHistories(clinicalHistories);
+		lists.setDoctors(doctors);
+		lists.setIllnesses(illnesses);
+		lists.setPatients(patients);
+		lists.setSurgeries(surgeries);
+		lists.setTreatments(treatments);
+		lists.setVaccines(vaccines);
+	
 		marshaller.marshallLists(lists, file);
 	}
 
