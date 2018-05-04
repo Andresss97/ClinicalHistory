c1ca86539eafe44941d2906b388a2da0bfade9f3
Daniel Marchan <danielmarchan3@gmail.com>
2018-05-04 17:12:58 +0200

    import

--- src/xml/DataBaseUnmarshaller.java
+++ src/xml/DataBaseUnmarshaller.java
@@ -8,7 +8,7 @@ public class DataBaseUnmarshaller {
 	private JAXBContext jaxbContext;
 	private Unmarshaller unmarshaller;
 	
-	private Appointment unmarshallAppointment (File file) throws JAXBException {
+	public Appointment unmarshallAppointment (File file) throws JAXBException {
 		jaxbContext = JAXBContext.newInstance(Appointment.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
@@ -16,76 +16,85 @@ public class DataBaseUnmarshaller {
 		return appointment;
 	}
 	
-	private Allergies unmarshallAllergy (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Allergies unmarshallAllergy (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Allergies.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Allergies allergy = (Allergies) unmarshaller.unmarshal(file);
 		return allergy;
 	}
 	
-	private Address unmarshallAddress (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Address unmarshallAddress (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Address.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Address address = (Address) unmarshaller.unmarshal(file);
 		return address;
 	}
 	
-	private ClinicalHistory unmarshallClinicalHistory (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public ClinicalHistory unmarshallClinicalHistory (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(ClinicalHistory.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		ClinicalHistory clinicalHistory = (ClinicalHistory) unmarshaller.unmarshal(file);
 		return clinicalHistory;
 	}
 	
-	private Doctor unmarshallDoctor (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Doctor unmarshallDoctor (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Doctor.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Doctor doctor = (Doctor) unmarshaller.unmarshal(file);
 		return doctor;
 	}
 	
-	private Illness unmarshallIllness (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Illness unmarshallIllness (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Illness.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Illness illness = (Illness) unmarshaller.unmarshal(file);
 		return illness;
 	}
 	
-	private Patient unmarshallPatient (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Patient unmarshallPatient (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Patient.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Patient patient = (Patient) unmarshaller.unmarshal(file);
 		return patient;
 	}
 	
-	private Surgeries unmarshallSurgery (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Surgeries unmarshallSurgery (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Surgeries.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Surgeries surgery = (Surgeries) unmarshaller.unmarshal(file);
 		return surgery;
 	}
 	
-	private Treatment unmarshallTreatment (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Treatment unmarshallTreatment (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Treatment.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Treatment treatment = (Treatment) unmarshaller.unmarshal(file);
 		return treatment;
 	}
 	
-	private Vaccine unmarshallVaccine (File file) throws JAXBException {
-		jaxbContext = JAXBContext.newInstance(Appointment.class);
+	public Vaccine unmarshallVaccine (File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(Vaccine.class);
 		unmarshaller = jaxbContext.createUnmarshaller();
 		
 		Vaccine vaccine = (Vaccine) unmarshaller.unmarshal(file);
 		return vaccine;
 	}
+	
+	public XmlLists unmarshallXmL(File file) throws JAXBException {
+		jaxbContext = JAXBContext.newInstance(XmlLists.class);
+		unmarshaller = jaxbContext.createUnmarshaller();
+		
+		XmlLists xmllists =(XmlLists)unmarshaller.unmarshal(file);
+		return xmllists;
+	}
+	
 }
 
--- src/xml/ImportDataBase.java
+++ src/xml/ImportDataBase.java
@@ -1,5 +1,24 @@
 package xml;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import pojos.*;
+
 public class ImportDataBase {
+	
+	private DataBaseUnmarshaller databaseunmarshaller;
+	
+	public XmlLists importDataBase (File file) throws JAXBException {
+		
+		XmlLists database = (XmlLists)databaseunmarshaller.unmarshallXmL(file);
+				
+		
+		
+		
+		
+		return database;
+	}
 
 }
